924797aeebcba58a14241fae42740bba34bf6dea
goes0107 <maxaleixo169@gmail.com>
2026-03-27 15:42:24 -0300

    Resolução da questão 1

--- src/atividadesTS.ts
+++ src/atividadesTS.ts
@@ -1,2 +1,15 @@
-// 2. Crie uma função que receba um número e retorne se ele é par ou ímpar.
+// 1. Crie um programa que receba um número e informe se ele é positivo, negativo ou zero.
 
+let num: number = Number(window.prompt(`Insira um número: `))
+
+if (num >= 1){
+    document.writeln(`O número informado é positivo`)
+}
+
+else if(num < 0){
+    document.writeln(`O número informado é negativo`)
+}
+
+else if (num == 0){
+    document.writeln(`O número informado é 0`)
+}
