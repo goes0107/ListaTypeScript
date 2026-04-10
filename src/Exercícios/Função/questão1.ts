3551f278da8c8287d1ae5c9876c4d109debac1da
goes0107 <maxaleixo169@gmail.com>
2026-04-10 15:25:42 -0300

    subindo a pasta função e a questão 1 de função

---
+++ src/Exercícios/Função/questão1.ts
@@ -0,0 +1,16 @@
+// 1. Crie uma função somarAte que recebe um número inteiro positivo. Use um laço de repetição
+// para calcular a soma de todos os números de 1 até o número fornecido. (Ex: somarAte(4)
+// deve retornar 10, pois 1+2+3+4 = 10).
+
+let soma: number = 0
+
+function somarAte (num: number): any{
+    for (let i = 1; i <= num; i++){
+        soma = soma + i
+    }
+    document.writeln(`Resultado da soma: ${soma}`)
+}
+
+let numero: number = Number(window.prompt(`Insira um número inteiro positivo`))
+
+somarAte(numero)
