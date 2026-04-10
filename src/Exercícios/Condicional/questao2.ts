15d2a819a820cd6d2e51ac1a45afc7355525a6cd
goes0107 <maxaleixo169@gmail.com>
2026-04-10 15:01:30 -0300

    subindo a questão 2 condicional

--- src/Exercícios/Condicional/questao2.ts
+++ src/Exercícios/Condicional/questao2.ts
@@ -1,1 +1,34 @@
-oi
+// 2. Crie um programa chamado Estações. Este programa deve ler uma data e armazenar na
+// variável mês, um número entre 1 e 12, correspondendo a um dos meses do ano. No final,
+// você deve imprimir uma mensagem conforme o exemplo: &quot;A estação do ano correspondente
+// ao mês 3 é Verão&quot; Considere a estação prevalente para cada mês:
+// a) Janeiro (1): Verão
+// b) Fevereiro (2): Verão
+// c) Março (3): Verão
+// d) Abril (4): Outono
+// e) Maio (5): Outono
+// f) Junho (6): Outono
+// g) Julho (7): Inverno
+// h) Agosto (8): Inverno
+// i) Setembro (9): Inverno
+// j) Outubro (10): Primavera
+// k) Novembro (11): Primavera
+// l) Dezembro (12): Primavera
+
+let mes: number = Number(window.prompt(`Digite um número entre 1 e 12 que represente cada mês do ano.`))
+
+if ((mes == 1) || (mes == 2) || (mes == 3)){
+    document.writeln(`A estação do ano correspondente ao mês ${mes} é Verão`)
+}
+
+else if ((mes == 4) || (mes == 5) || (mes == 6)){
+    document.writeln(`A estação do ano correspondente ao mês ${mes} é Outono`)
+}
+
+else if ((mes == 7) || (mes == 8) || (mes == 9)){
+    document.writeln(`A estação do ano correspondente ao mês ${mes} é Inverno`)
+}
+
+else if ((mes == 10) || (mes == 11) || (mes == 12)){
+    document.writeln(`A estação do ano correspondente ao mês ${mes} é Primavera`)
+}
