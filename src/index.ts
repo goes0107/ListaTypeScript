6658589a140a7f4fd6fa840aae1e4ea2ea8870de
goes0107 <maxaleixo169@gmail.com>
2026-04-17 10:20:57 -0300

    correção dos botões

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 // QUESTÕES CONDICIONAL
-import { questao1condicional } from "./Exercícios/Condicional/questão1";
-import { questao2condicional } from "./Exercícios/Condicional/questao2";
+import { questao1condicional } from "./Exercícios/Condicional/questão1.js";
+import { questao2condicional } from "./Exercícios/Condicional/questao2.js";
 
-import { questao4condicional } from "./Exercícios/Condicional/questão4";
+import { questao4condicional } from "./Exercícios/Condicional/questão4.js";
 
-document.getElementById("btn1")?.addEventListener("click", questao1condicional)
-document.getElementById("btr2")?.addEventListener("click", questao2condicional)
+document.getElementById("btnEx1.con")?.addEventListener("click", questao1condicional)
+document.getElementById("btnEx2.con")?.addEventListener("click", questao2condicional)
 
-document.getElementById("btr4")?.addEventListener("click", questao4condicional)
+document.getElementById("btnEx4.con")?.addEventListener("click", questao4condicional)
 
 // FUNÇÃO
-import { questao1funcao } from "./Exercícios/Função/questão1";
+import { questao1funcao } from "./Exercícios/Função/questão1.js";
 
 
 
-document.getElementById("btr5")?.addEventListener("click", questao1funcao)
+document.getElementById("btnEx1.fun")?.addEventListener("click", questao1funcao)
