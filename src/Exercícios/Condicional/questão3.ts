05c4a47dfa38ecacc60e9c0b227b43cb2af4138c
goes0107 <maxaleixo169@gmail.com>
2026-04-17 10:40:06 -0300

    subindo a questão 3 condicional

---
+++ src/Exercícios/Condicional/questão3.ts
@@ -0,0 +1,31 @@
+// 3. Crie um programa que solicite dois números e simule um menu de uma calculadora:
+// 1 - Soma
+// 2 - Subtração
+// 3 - Multiplicação
+// 4 - Divisão
+// Use switch...Case
+
+export function questao3condicioal(): void{
+    let calculadora: number = Number(prompt(`---CALCULADORA--- \n 1- Adição \n 2- Subtração \n 3- Multiplicação \n 4- Divisão \n Escolha uma opção: `))
+    let num1: number = Number(prompt(`Insira o primeiro número: `))
+    let num2: number = Number(prompt(`Insira o segundo número: `))
+
+    switch(calculadora){
+        case 1:
+            let soma: number = num1 + num2
+            document.writeln(`Soma dos números digitados: ${soma}`)
+            break
+        case 2:
+            let subtracao: number = num1 - num2
+            document.writeln(`Subtração dos números digitados: ${subtracao}`)
+            break
+        case 3:
+            let multiplicacao: number = num1 * num2
+            document.writeln(`Multiplicação dos números digitados: ${multiplicacao}`)
+            break
+        case 4:
+            let divisao: number = num1 / num2
+            document.writeln(`Divisão dos números digitados: ${divisao}`)
+            break
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // QUESTÕES CONDICIONAL
 import { questao1condicional } from "./Exercícios/Condicional/questão1.js";
 import { questao2condicional } from "./Exercícios/Condicional/questao2.js";
-
+import { questao3condicioal } from "./Exercícios/Condicional/questão3.js";
 import { questao4condicional } from "./Exercícios/Condicional/questão4.js";
 
 document.getElementById("btnEx1.con")?.addEventListener("click", questao1condicional)
 document.getElementById("btnEx2.con")?.addEventListener("click", questao2condicional)
-
+document.getElementById("btnEx3.con")?.addEventListener("click", questao3condicioal)
 document.getElementById("btnEx4.con")?.addEventListener("click", questao4condicional)
 
 // FUNÇÃO
