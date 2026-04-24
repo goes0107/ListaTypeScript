78717f8944bd032040f7be087d60a4868e1fc201
goes0107 <maxaleixo169@gmail.com>
2026-04-24 10:46:29 -0300

    Pequena mudança na questão 7 de função

--- src/Exercícios/Função/questao7.ts
+++ src/Exercícios/Função/questao7.ts
@@ -16,11 +16,11 @@ export function questao7funcao():void{
         return autonomia
     }
 
-    let dispercorrida: number = Number(prompt(`Insira a distância total percorrida em km: `))
+    let disPercorrida: number = Number(prompt(`Insira a distância total percorrida em km: `))
     let combGasto: number = Number(prompt(`Insira o total de combustível gasto em litros: `))
     let qntTotalTanque: number = Number(prompt(`Insira a capacidade total do tanque em litros: `))
 
-    let consuMedio = consumoMedio(dispercorrida, combGasto)
+    let consuMedio = consumoMedio(disPercorrida, combGasto)
     let autonomiaCarro = autonomia(consuMedio, qntTotalTanque)
     console.log(`O consumo médio do carro é de ${consuMedio.toFixed(2)} km/l`)
     console.log(`A autonomia do carro com o tanque cheio é de ${autonomiaCarro.toFixed(2)} km`)
